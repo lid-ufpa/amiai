0577624a9e3f7eb61b3266cdbc54f0cdc159629c
msiqueiras <devef.msiqueira@gmail.com>
2026-03-28 19:30:39 -0300

    feat: Create a vectorization notebook and function in src

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,8 +2,10 @@ import spacy
 import unidecode
 from nltk.corpus import stopwords
 from nltk.stem.rslp import RSLPStemmer  
+import numpy as np
 
-nlp = spacy.load('pt_core_news_sm')
+
+nlp = spacy.load('pt_core_news_md')
 stopwords = list(stopwords.words('portuguese'))
 PT_STOPWORDS = [unidecode.unidecode(word) for word in stopwords]
 STEMMER = RSLPStemmer()
@@ -42,3 +44,25 @@ def lemmatization(text):
         lemmas.append(txt.lemma_)
 
     return lemmas
+
+
+def vectorization_docs(series):
+
+    vectors_document = [] 
+
+    for lemmas_list in series:
+        vectors_words = []
+        for lemma in lemmas_list:
+            if nlp.vocab.has_vector(lemma):
+                vectors_words.append(nlp.vocab[lemma].vector)
+        
+        if vectors_words:
+
+            mean_vector = np.mean(vectors_words, axis=0)
+            vectors_document.append(mean_vector)
+        
+        else:
+            vectors_document.append(np.zeros(300)) #para caso não haja um vetor útil
+
+    
+    return np.array(vectors_document)
